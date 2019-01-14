79a87b62ae3a45fcd07b00f7c8d1ee37f16b1898
Lisa Stasio <lstasio@sgintl.com>
2019-01-14 13:47:53 -0500

    added more cards to page 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,8 @@
 
     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=ADDBLUEJAYLOOKUP";
 
+    <iframe src="https://giphy.com/embed/JLzpbdDNjKxj2" width="480" height="474" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sobre-rama-movindose-JLzpbdDNjKxj2">via GIPHY</a></p>
+
     // AJAX GET request to our queryURL
     $.ajax({
         url: queryURL,
